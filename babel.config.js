9366efddfdbdc474504f6fed90750a0ff3bf81e5
Vitor Paiva <paiva.v@icloud.com>
2023-01-24 14:42:32 -0300

    build :hammer_and_wrench:(): configure module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@components': './src/components',
+            '@themes': './src/global/themes',
+            '@configs': './src/configs',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@assets': './src/assets',
+            '@slices': './src/slices',
+            '@api': './src/api'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
